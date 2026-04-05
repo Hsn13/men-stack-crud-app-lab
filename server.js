03cf24ddbcdf663ca801a2fed3e2f23992ec8f80
Hsn13 <hkhosro5@gmail.com>
2026-04-06 02:11:17 +0300

    Practice

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
+const Car = require("./models/Car");
+const methodOverride = require("method-override");
 
+
+
+app.use(methodOverride("_method"));
 app.use(morgan('dev'));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
@@ -25,8 +30,30 @@ connectDB();
 
 //Routes
 
-app.get("/", (req, res) => {
-  res.render("homepage"); 
+app.get('/cars/new', (req, res)=>{
+  const categories = Car.schema.path('category').enumValues;
+  res.render('new', { categories });
+});
+
+app.post("/new", async (req, res) => {
+   try {
+     await Car.create(req.body);
+     res.redirect("/");
+   } catch (error) {
+     console.error(error);
+     res.status(400).send("Error creating car: " + error.message);
+   }
+});
+
+app.get("/", async (req, res) => {
+  const allData = await Car.find({});
+  res.render("homepage",{allData : allData}); 
+});
+
+app.get("/cars/:id", async (req, res) => {
+  const car = await Car.findById(req.params.id);
+  const categories = Car.schema.path('category').enumValues;
+  res.render("edit", { car, categories });
 });
 
 app.get('/test',  (req, res )=>{
@@ -34,6 +61,24 @@ console.log('test');
 res.render('homepage');
 });
 
+app.put('/cars/:id', async (req, res) => {
+  try {
+    const car = await Car.findById(req.params.id);
+    car.modelName = req.body.modelName;
+    car.year = req.body.year;
+    car.category = req.body.category;
+    await car.save();
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Error updating car: " + error.message);
+  }
+});
+
+app.post('/cars/:id/delete', async (req, res) =>{
+  await Car.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
 
 app.listen(3000,()=>
   {console.log("Connected successfuly to port 3000")
